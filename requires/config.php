7a8958ce6e27819ca7bab3ca87c9427bed9bbc37
Christohero <103386102+Christohero@users.noreply.github.com>
2023-02-10 17:22:09 -0500

    Update config.php

--- requires/config.php
+++ requires/config.php
@@ -35,7 +35,7 @@
     $vehicles_db = "characters_vehicles";
     $bills_db = "characters_bills";
 
-    $licensekey = "MDT1T70EWO7K6D"; // Licensekey on pepe-framework.com
+    $licensekey = "MDT1270UBQIBY0AGO"; // Licensekey on pepe-framework.com
     $username = "admin"; // Username on pepe-framework.com
 
     $webname = '<span class="text-danger">H</span><span class="text-warning">i</span><span class="text-success">g</span><span class="text-info">h</span><span class="text-royal-orange">-</span><span class="text-jungle-green">M</span><span class="text-jungle-warning">e</span><span class="text-jungle-error">o</span><span class="text-jungle-info">s</span>';
